e20e7ab101a2b305bf3ebfb8d69df104ca0606e6
Jayanthipalanichamy <jayanthi.palanichamy1997@gmail.com>
2018-06-12 19:15:16 +0530

    Initializing mars rover

--- MarsRover.js
+++ MarsRover.js
@@ -1,8 +1,10 @@
-var MarsRover = function () {
-
+var MarsRover = function (xPosition,yPosition,direction) {
+    this.xPosition = xPosition;
+    this.yPosition = yPosition;
+    this.direction = direction;
 };
 
 MarsRover.prototype.givePosition = function () {
-    return
+    return this.xPosition +" "+this.yPosition+" "+this.direction;
 };
 module.exports = MarsRover;
--- MarsRover.specs.js
+++ MarsRover.specs.js
@@ -2,7 +2,7 @@ var MarsRover = require('./MarsRover');
 
     describe('Check MarsRover Moves correctly.', function () {
         it('should be able to give its original Position', function () {
-            var marsRover = new MarsRover();
+            var marsRover = new MarsRover(1,2,'N');
 
             expect(marsRover.givePosition()).toEqual("1 2 N");
         });
